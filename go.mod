a515357c01a80549dcdaf198c7bcbab124e55a08
Hoa Le <hoale@Hoas-MacBook-Pro.local>
2026-05-23 12:18:06 +0700

    custom client query contract at specific block

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/alecthomas/participle/v2 v2.0.0-beta.5
 	github.com/oasisprotocol/curve25519-voi v0.0.0-20220328075252-7dd334e3daae
 	github.com/snksoft/crc v1.1.0
+	github.com/stretchr/testify v1.11.1
 	golang.org/x/crypto v0.45.0
 	golang.org/x/exp v0.0.0-20230116083435-1de6713980de
 	google.golang.org/grpc v1.77.0
@@ -18,7 +19,6 @@ require (
 	github.com/alecthomas/repr v0.1.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/testify v1.11.1 // indirect
 	golang.org/x/net v0.47.0 // indirect
 	golang.org/x/sys v0.38.0 // indirect
 	golang.org/x/text v0.31.0 // indirect
---
+++ liteapi/custom_client.go
@@ -0,0 +1,61 @@
+package liteapi
+
+import (
+	"context"
+
+	"github.com/tonkeeper/tongo/boc"
+	"github.com/tonkeeper/tongo/liteclient"
+	"github.com/tonkeeper/tongo/tlb"
+	"github.com/tonkeeper/tongo/ton"
+	"github.com/tonkeeper/tongo/utils"
+)
+
+func (c *Client) RunSmcMethodByIDAtBlock(ctx context.Context, accountID ton.AccountID, methodID int, params tlb.VmStack, block ton.BlockIDExt) (uint32, tlb.VmStack, error) {
+	cell := boc.NewCell()
+	err := tlb.Marshal(cell, params)
+	if err != nil {
+		return 0, tlb.VmStack{}, err
+	}
+	b, err := cell.ToBoc()
+	if err != nil {
+		return 0, tlb.VmStack{}, err
+	}
+	client, _, err := c.pool.BestClientByAccountID(ctx, accountID, true)
+	if err != nil {
+		return 0, tlb.VmStack{}, err
+	}
+	req := liteclient.LiteServerRunSmcMethodRequest{
+		Mode:     4,
+		Id:       liteclient.BlockIDExt(block),
+		Account:  liteclient.AccountID(accountID),
+		MethodId: uint64(methodID),
+		Params:   b,
+	}
+	res, err := client.LiteServerRunSmcMethod(ctx, req)
+	if err != nil {
+		return 0, tlb.VmStack{}, err
+	}
+	var result tlb.VmStack
+	if res.ExitCode == 4294967040 { //-256
+		return res.ExitCode, tlb.VmStack{}, ErrAccountNotFound
+	}
+	cells, err := boc.DeserializeBoc(res.Result)
+	if err != nil {
+		return 0, tlb.VmStack{}, err
+	}
+	if len(cells) != 1 {
+		return 0, tlb.VmStack{}, boc.ErrNotSingleRoot
+	}
+	err = tlb.Unmarshal(cells[0], &result)
+	return res.ExitCode, result, err
+}
+
+func (c *Client) RunSmcMethodAtBlock(
+	ctx context.Context,
+	accountID ton.AccountID,
+	method string,
+	params tlb.VmStack,
+	block ton.BlockIDExt,
+) (uint32, tlb.VmStack, error) {
+	return c.RunSmcMethodByIDAtBlock(ctx, accountID, utils.MethodIdFromName(method), params, block)
+}
